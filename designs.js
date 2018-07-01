b960616ccec1e34148d06c504c7f70362241ae96
Frankmakz <frankosro60@gmail.com>
2018-07-01 18:05:33 +0300

    First commit

--- designs.js
+++ designs.js
@@ -1,31 +1,35 @@
 
-$('document').ready(function() {
-$('#sizePicker').submit(function makeGrid(grid) {
-	$('table tr').remove(); //prevents forming another table once you click submit
-	var rows = $('#inputHeight').val();
-	var cols = $('#inputWidth').val();
+var height, width, color;
 
-    //forms the grid
-	for(var i = 1; i<=rows ;i++) {
-		$('table').append('<tr></tr>');
-		for(j = 1; j<=cols ; j++) {
-			$("tr:last").append('<td></td>');
-			$('td').attr("class" , 'cells');
-
-		}
-	}
-	grid.preventDefault(); //prevent grid from disappearing
+//when size is submitted by the user ,call makeGrid()
+$('#sizePicker').submit(function (event) {
+   event.preventDefault();
+   height = $('#inputHeight').val();
+   width = $('#inputWeight').val();
+   makeGrid(height , width);
     
-    //inputs color on clicking cell
-	$('.cells').click(function(event) {
+
+});
+
+ function makeGrid(x , y){
+ 	$('tr').remove(); //removes grid on clicking submit 
+   //forms grid 
+ 	for (var i = 1; i<=x; i++){
+ 		$('#pixelCanvas').append('<tr id=row' + i + '></tr');
+        for (var j = 1; j <= y; j++) {
+           $('#row'+ i).append('<td></td>'); 
+        	$('td').attr("class" ,'cells')
+        	
+        }
+ 	}
+      //inputs color on clicking cell
+ 	$('.cells').click(function(event) {
 		var paint = $('#colorPicker').val();
 		$(event.target).css('background-color' , paint);
 	});
 
- });
-});
-
  
+ }
 
 
 
